58d20a576bdaca647d799ed7330578dcf7f8331a
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-16 18:47:19 +0200

    Esercizio array-lista svolto creando lista frutta e metodo per controllare se il cocomero é nel frigo

--- array-lista/JS/script.js
+++ array-lista/JS/script.js
@@ -1,14 +1,9 @@
-const myNumber = Number.parseInt (Math.random() * 6 + 1)
-const pcNumber = Number.parseInt (Math.random() * 6 + 1)
-if(myNumber === pcNumber){
-    console.log("Un pareggi non é contemplato, ricarica la pagina!")
-    console.log(myNumber,  pcNumber)
+const listFridge = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
+listFridge.push ('pesca')
+let find = "Oh no, devo uscire a comprare il cocomero!"
+for(let i = 0; i < listFridge.length; i++){
+    if('cocomero' === listFridge[i]){
+        find = "Trovato! Devo solo preparare il cocktail."
+    }
 }
-else if (myNumber > pcNumber){
-    console.log("L'uomo ha vinto sulla macchina!")
-    console.log(myNumber,  pcNumber)
-}
-else{
-    console.log("Le macchine sono piu forti!")
-    console.log(myNumber,  pcNumber)
-}
+console.log(find)
